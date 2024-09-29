e4893c0b37dc1004f7a04a1344c7745a0d75fbdf
kartikay-agarwal <kartikayagarwal10@gmail.com>
2024-09-29 16:28:31 +0530

    modified:   test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-# password = "yourpassword"  # Example password
-# lower = "abcdefghijklmnopqrstuvwxyz"  # Lowercase letters
-# check = 0  # Initialize check variable
+password = "yourpassword"  # Example password
+lower = "abcdefghijklmnopqrstuvwxyz"  # Lowercase letters
+check = 0  # Initialize check variable
 
-# for i in password:
-#     if i.islower():
-#         check = check + 1
-#         break
+for i in password:
+    if i.islower():
+        check = check + 1
+        break
 
-# print(f"Check value: {check}")
+print(f"Check value: {check}")
